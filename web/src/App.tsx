8eee7f118560efc5874c803868e36a9937705fc3
Bruno Santos <brcesar19@gmail.com>
2023-09-13 09:51:26 -0300

    chore: add shadcn-ui; tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,23 @@
+import { Github } from 'lucide-react';
+import { Button } from './components/ui/button';
+import { Separator } from './components/ui/separator';
+
 export function App() {
 
   return (
-    <>
-      
-    </>
-  )
+    <div className='min-h-screen flex flex-col'>
+      <div className='flex items-center justify-between py-3 px-6 border-b'>
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>Desenvolvido com love no NLW</span>
+          <Separator orientation='vertical' className='h-6' />
+          <Button variant='outline'>
+            <Github className='w-4 h-4 mr-2' />
+            Github
+          </Button>
+        </div>
+      </div>
+      <div className='flex-1 p-6'>oi</div>
+    </div>
+  );
 }
